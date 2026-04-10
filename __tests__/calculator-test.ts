14904f922b9083883c31005af651798a3e3cefa0
Andy Meagher <andy@chaninicholas.com>
2026-04-10 15:25:37 -0400

    more tests

--- __tests__/calculator-test.ts
+++ __tests__/calculator-test.ts
@@ -9,13 +9,30 @@ import { calculateBackingOptions } from "@/utils/calculator";
 // };
 
 describe('Backing Calculator', () => {
-  it('Calculates Correctly', () => {
+  it('Calculates Normal Dimensions Correctly', () => {
     const optionsList = calculateBackingOptions({quiltLength: "54", quiltWidth: "45", backingWidth: "42", backingMargin: "4", units: "inches"})
     const normalOption = optionsList.options.find(item =>{
      return item.seamDirection != "diagonal"
     })
     expect(normalOption?.seams).toBe(1); 
     expect(normalOption?.total).toBe(3); 
+  });
 
+  it('Calculates Large Dimensions Correctly', () => {
+    const optionsList = calculateBackingOptions({quiltLength: "110", quiltWidth: "100", backingWidth: "42", backingMargin: "2", units: "inches"})
+    const normalOption = optionsList.options.find(item =>{
+     return item.seamDirection != "diagonal"
+    })
+    expect(normalOption?.seams).toBe(2); 
+    expect(normalOption?.total).toBe(8.75); 
+  });
+
+  it('Calculates Extra Small Dimensions Correctly', () => {
+    const optionsList = calculateBackingOptions({quiltLength: "5", quiltWidth: "4", backingWidth: "4", backingMargin: "0", units: "inches"})
+    const normalOption = optionsList.options.find(item =>{
+     return item.seamDirection != "diagonal"
+    })
+    expect(normalOption?.seams).toBe(0); 
+    expect(normalOption?.total).toBe(0.25); 
   });
 });
